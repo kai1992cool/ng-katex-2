5b1fc8efb0268233042ad97f96644fe09f5acb3f
Naren <kai1992cool@gmail.com>
2025-05-03 14:05:07 +0000

    dev

--- dist/ng-katex-2/public-api.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './lib/ng-katex.component';
-export * from './lib/ng-katex-paragraph.component';
-export * from './lib/ng-katex.module';
-export * from './lib/ng-katex.service';
-export * from './lib/ng-katex.directive';
-export * from './lib/ng-katex-html.component';
